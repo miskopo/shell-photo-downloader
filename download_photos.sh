ff8b4bb9f00de22158210efbe3a1a4fa2e4dba87
Michal Polovka <michal.polovka@gmail.com>
2019-01-07 11:04:42 +0100

    Fixed var name bug and added opp to unmount card
    Signed-off-by: Michal Polovka <michal.polovka@gmail.com>

--- download_photos.sh
+++ download_photos.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 # Define camera manufacturer
-CAMERA_MODEL="NIKON D7200"
+CAMERA_MODEL="NIKON D7200"  # SD card formatted in camera is usually named like this
 # Define target base directory
 TARGET_PATH="/home/$USER/Pictures/Nikon"
 
+# Color definitions
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
 # Check if SD card is in computer
-if [ -d "/run/media/$USER/$MANUFACTURER" ]; then
+if [[ -d "/run/media/$USER/$CAMERA_MODEL" ]]; then
 	SD_PATH="/run/media/$USER/$CAMERA_MODEL";
 	echo "SD card detected at $SD_PATH";
 else
@@ -20,24 +26,43 @@ fi
 INNER_FOLDER=$(ls)
 [[ -v INNER_FOLDER ]] && cd "$INNER_FOLDER" || $(echo "Invalid directory structure on SD card"; exit 1);
 
+
+# Init error count
+ERROR_COUNT=0
+
 # Securely copy photos to respective folders
 for FILE in $(ls); do
 	TAKEN_DATE=$(exiftool -p '$dateTimeOriginal' -d "%Y/%m/%d" "$FILE");
-	YEAR=$(echo $TAKEN_DATE | cut -d "/" -f1);
-	MONTH=$(echo $TAKEN_DATE | cut -d "/" -f2);
-	DAY=$(echo $TAKEN_DATE | cut -d "/" -f3);
+
+	YEAR=$(echo ${TAKEN_DATE} | cut -d "/" -f1);
+	MONTH=$(echo ${TAKEN_DATE} | cut -d "/" -f2);
+	DAY=$(echo ${TAKEN_DATE} | cut -d "/" -f3);
+
 	FULL_TARGET_PATH="$TARGET_PATH/$YEAR/$MONTH/$DAY"
+
 	echo "Copying $FILE to $FULL_TARGET_PATH"
-		if [ ${FILE: -4} == ".JPG" ]; then
+		if [[ ${FILE: -4} == ".JPG" ]]; then
 			mkdir -p "$FULL_TARGET_PATH/JPG" && 
-			rsync -ahqu --progress $FILE "$FULL_TARGET_PATH/JPG"||
+			rsync -ahqu --progress ${FILE} "$FULL_TARGET_PATH/JPG"||
 			echo "Error copying $FILE to $FULL_TARGET_PATH/JPG";
 		else
 			mkdir -p "$FULL_TARGET_PATH" && 
-			rsync -ahqu --progress $FILE "$FULL_TARGET_PATH"||
-			echo "Error copying $FILE to $FULL_TARGET_PATH";
-		fi
+			rsync -ahqu --progress ${FILE} "$FULL_TARGET_PATH"||
+			echo "${RED}Error copying $FILE to $FULL_TARGET_PATH${NC}";
+            ((++ERROR_COUNT));
+        fi
 	done;
 
 # All done, inform user
-echo "Copying done"
+echo "Copying done, ${ERROR_COUNT} errors occurred."
+
+# Give option to unmount card
+echo "Would you like to unmount the SD card now? "
+read -p '([y]/n)' USER_RESPONSE
+
+if [[ ${USER_RESPONSE} = '\n' || ${USER_RESPONSE} = 'y' ]]; then
+    umount "/run/media/$USER/$CAMERA_MODEL";
+    echo "You can detach the SD card now";
+else
+    exit 0;
+fi
